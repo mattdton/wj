e78c838b437c94c659b18f7e3e94ca35857e08dc
Matthew Downton <4353654+tdm-dev@users.noreply.github.com>
2018-04-20 11:11:40 +1000

    Outline of cli interface.

---
+++ wj_process.py
@@ -0,0 +1,57 @@
+import sys,os
+import datetime
+from argparse import ArgumentParser
+
+fname = os.environ["WJ_FILENAME"]
+
+def parse_args():
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(help='commands')
+
+    add_parser = subparsers.add_parser('add',
+                                       help='Add a new entry for today.')
+
+    add_parser.add_argument('entry',
+                            action='store',
+                            type=str,
+                            help="New entry in the form: 'Sentence. @tag1 @tag2'")
+    add_parser.set_defaults(func=addEntry)
+
+    tag_parser = subparsers.add_parser('tag',
+                                      help='Print entries for a given tag.')
+    tag_parser.add_argument('tagID',
+                            action='store',
+                            type=str,
+                            help='tag to print')
+    tag_parser.set_defaults(func=printTagEntries)
+
+    today_parser = subparsers.add_parser('today',
+                                         help='Print entries for today.')
+    today_parser.set_defaults(func=printTodayEntries)
+    
+    yesterday_parser = subparsers.add_parser('yesterday',
+                                             help='Print entries for yesterday.')
+    yesterday_parser.set_defaults(func=printYesterdayEntries)
+
+    args = parser.parse_args()
+    return args
+
+def addEntry():
+    return
+
+def printTagEntries():
+    return
+
+def printTodayEntries():
+    return
+
+def printYesterdayEntries():
+    return
+
+def main():
+    print('Opening %s'%fname)
+
+    args = parse_args()
+
+if __name__ == '__main__':
+    main()
