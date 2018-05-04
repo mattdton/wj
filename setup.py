908cbc19f0fe3c1acdeee4648d5f5fea3727522f
Matthew Downton <4353654+tdm-dev@users.noreply.github.com>
2018-05-04 19:43:47 +1000

    Reorganise directory structure. Closes #1.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wj',
+      version='0.1',
+      description='Tool for adding to and querying plain text task tracking files.',
+      url='https://github.com/tdm-dev/wj',
+      scripts=['scripts/wj'],
+      author='Matthew Downton',
+      license='MIT',
+      packages=['wj'])
+      
