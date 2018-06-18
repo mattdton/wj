22276d504bf8e8cd2d4124180a1cda434f2eab09
Matthew Downton <4353654+tdm-dev@users.noreply.github.com>
2018-06-18 14:49:16 +1000

    Basic printing of a plain text calendar.

--- wj/cal.py
+++ wj/cal.py
@@ -2,7 +2,11 @@ import datetime
 
 _dayName = {1:'Mo',2:'Tu',3:'We',4:'Th',5:'Fr',6:'Sa',7:'Su'}
 
-def _lineHead(nday=37):
+def _title(year,month):
+    date = datetime.date(year,month,1)
+    return '{0:^21}'.format(date.strftime('%B'))
+
+def _dayHead(nday=37):
     out = ''
     for i in range(nday):
         out = out+' '+_dayName[i%7+1]
@@ -19,4 +23,24 @@ def _month2str(year,month):
     return out
 
 def _chopMonthString(s):
-    return [s[20*start:20*(start+1)] for start in range(5)]
+    return ['{0: <21}'.format(s[21*i:21*(i+1)]) for i in range(6)]
+
+def composeMonth(year,month):
+    """Format the dates in a month as a small block of text with a line
+    for each week. Returns a list where each item is one of the lines.
+    """
+    output = [_title(year,month),_dayHead(7)]
+    output.extend(_chopMonthString(_month2str(year,month)))
+    return header
+
+def printYear(year):
+    """Print the calendar for a year with four months on each row."""
+    months = [composeMonth(year,month) for month in range(1,13)]
+    for group in range(3):
+        index = 4*group
+        for line in range(8):
+            print(months[index][line],end='  ')
+            print(months[index+1][line],end='  ')
+            print(months[index+2][line],end='  ')
+            print(months[index+3][line],end='\n')
+        print('\n')
